8b773335fca2055b9d8609824b623a5cdc74ead5
김민수 <alstnalsgud@gmail.com>
2022-02-14 23:13:21 +0900

    Time: 3 ms (58.93%), Space: 49 MB (20.78%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int count = 0;
+        int current = 0;
+        int max = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++){
+            max = Math.max(max, nums[i] + i);
+            if (i == current){
+                count++;
+                current = max;
+            }
+        }
+        return count;
+    }
+    
+}
