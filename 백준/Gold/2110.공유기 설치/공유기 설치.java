4ae670affe8ca0b7221feeeefe15c5e10713b9f2
김민수 <alstnalsgud@gmail.com>
2022-02-26 17:21:10 +0900

    [Gold V] Title: 공유기 설치, Time: 412 ms, Memory: 60852 KB -BaekjoonHub

---
+++ 백준/Gold/2110.공유기 설치/공유기 설치.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, C;
+    static int[] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(map);
+        solution();
+    }
+    static void solution(){
+        long low = 0, high = 1000000000, ans = 0;
+        while (low <= high){
+            long mid = (low + high) / 2;
+            if (determination((int) mid)){
+                low = mid + 1;
+                ans = mid;
+            }else{
+                high = mid - 1;
+            }
+        }
+        System.out.println(ans);
+    }
+    static boolean determination(int gap){
+        int cnt = 1;
+        int[] selected = new int[N + 1];
+        selected[1] = map[1];
+        int selectedIdx = 1;
+        for (int i = 2; i < N + 1; i++) {
+            if (map[i] - selected[selectedIdx] >= gap){
+                ++cnt;
+                ++selectedIdx;
+                selected[selectedIdx] = map[i];
+            }
+
+        }
+        if (cnt >= C){
+            return true;
+        }
+        return false;
+    }
+}
