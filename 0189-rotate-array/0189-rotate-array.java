3ca32cfe5445b18d20d9578c7bb88045354c3759
김민수 <alstnalsgud@gmail.com>
2023-09-29 21:31:35 +0900

    Time: 1 ms (50.52%), Space: 55.8 MB (48.28%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -8,11 +8,9 @@ class Solution {
         for (int i = nums.length - k; i < nums.length; i++) {
             tempArray[-nums.length + k + i] = nums[i];
         }
-        System.out.println(Arrays.toString(tempArray));
         for (int i = nums.length - 1; i >= k; i--) {
             nums[i] = nums[i - k];
         }
-        System.out.println(Arrays.toString(nums));
         for (int i = 0; i < k; i++) {
             nums[i] = tempArray[i];
         }
