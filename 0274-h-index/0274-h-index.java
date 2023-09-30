1cf3451d09db94ace67bc7f21ac8ae10911bce89
김민수 <alstnalsgud@gmail.com>
2023-09-30 16:54:18 +0900

    Time: 15 ms (6.09%), Space: 41.1 MB (6.84%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    // 0 1 3 5 6
+    // 0 1 2 3 4 4
+    // 2 2 9 7
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int cnt = 0;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            int citation = citations[i];
+            if (cnt < citation) {
+                ++cnt;
+            }
+            System.out.println(cnt);
+        }
+        return cnt;
+    }
+}
