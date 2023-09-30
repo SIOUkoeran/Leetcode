bc728726844ed38aa9bab5e67687fd806e9634ac
김민수 <alstnalsgud@gmail.com>
2023-09-30 15:02:17 +0900

    Time: 302 ms (10.88%), Space: 44 MB (71.26%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1)
+            return true;
+        int result = 0;
+        int idx = 0;
+        while (idx < nums.length && result < nums.length) {
+            if (result < idx)
+                break;
+            result = Math.max(idx + nums[idx], result);
+            ++idx;
+            System.out.println(idx);
+        }
+        return result >= nums.length - 1 ? true : false;
+    }
+}
