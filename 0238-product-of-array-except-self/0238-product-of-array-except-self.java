45a1ebbd401ddf42d5f70601ceb6cfacfacefcae
김민수 <alstnalsgud@gmail.com>
2023-10-01 20:37:38 +0900

    Time: 178 ms (6.59%), Space: 52.9 MB (44.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.fill(forward, 1);
         Arrays.fill(backward, 1);
         
-        for (int i = 2; i < nums.length + 1; i++) { 
+        for (int i = 2; i < nums.length + 1; i++) {
             forward[i] = forward[i - 1]  * nums[i - 2];
             backward[nums.length - i + 1] = backward[nums.length - i + 2] * nums[nums.length + 1 - i];
         }
