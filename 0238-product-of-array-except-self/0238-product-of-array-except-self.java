188ca69d29d0634296135e434c62b24bc8add2db
김민수 <alstnalsgud@gmail.com>
2023-10-01 20:35:38 +0900

    Time: 165 ms (6.59%), Space: 52.8 MB (48.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] outputNums = new int[nums.length];
-        int[] postCalc = new int[nums.length];
-        int[] preCalc = new int[nums.length];
-        preCalc[0] = 1;
-        postCalc[nums.length - 1] = 1;
-        for (int i = 1; i < nums.length; i++) {
-            preCalc[i] = preCalc[i - 1] * nums[i - 1];
-            postCalc[nums.length - 1 - i] = nums[nums.length - i] * postCalc[nums.length -i];
-         }
+        int[] forward = new int[nums.length + 2];
+        int[] backward = new int[nums.length + 2];
+        for (int i = 0; i < nums.length + 2; i++) {
+            forward[i] = 1;
+            backward[i] = 1;
+        }
+        for (int i = 2; i < nums.length + 1; i++) {
+            forward[i] = forward[i - 1]  * nums[i - 2];
+            backward[nums.length - i + 1] = backward[nums.length - i + 2] * nums[nums.length + 1 - i];
+        }
         for (int i = 0; i < nums.length; i++) {
-            outputNums[i] = preCalc[i] * postCalc[i];
+            nums[i] = forward[i + 1] * backward[i + 1];
+            System.out.println(nums[i]);
         }
-        return outputNums;
-        }
+        return nums;
+    }
 }
