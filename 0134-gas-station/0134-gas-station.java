5a079ba3f403fcb9619a72fc5511bd64d7b41660
김민수 <alstnalsgud@gmail.com>
2023-10-01 20:47:22 +0900

    Time: 2 ms (92.23%), Space: 55.1 MB (78.06%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,17 +1,18 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int sum = 0;
-        int result = 0;
-        int resultIdx = 0;
+        int costTotal = 0;
+        int total = 0;
+        int startIdx = 0;
         for (int i = 0; i < gas.length; i++) {
-            sum += gas[i] - cost[i];
-            if (sum < 0) {
-                result += sum;
-                sum = 0;
-                resultIdx = i + 1;
+            costTotal += gas[i] - cost[i];
+            if (costTotal < 0) {
+                total += costTotal;
+                costTotal = 0;
+                startIdx = i + 1 > gas.length ? -1 : i + 1;
             }
         }
-        result += sum;
-        return result < 0 ? -1 : resultIdx;
+        if (costTotal > 0)
+            total += costTotal;
+        return total >= 0 ? startIdx : -1;
     }
 }
