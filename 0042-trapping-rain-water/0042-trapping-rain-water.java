9d03976d7aea928671e27d6a588b4bf2cd46cfb3
김민수 <alstnalsgud@gmail.com>
2023-10-02 21:35:52 +0900

    Time: 2 ms (16.52%), Space: 44.1 MB (74.32%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int trap(int[] height) {
+        int result = 0;
+        int preWall = height[0];
+        int[] water = new int[height.length];
+        
+        int prevWall = height[0];
+        //fill water
+        for (int i = 1; i < height.length; i++) {
+            if (prevWall < height[i]) {
+                prevWall = height[i];
+            }
+            if (height[i] < prevWall) {
+                result += prevWall - height[i];
+                water[i] = prevWall - height[i];
+            }
+        }
+        prevWall = height[height.length - 1];
+        
+        //pour water
+        for (int i = height.length - 1; i >= 0; i--) {
+            if (prevWall < height[i]) {
+                prevWall = height[i];
+            }
+            if (height[i] + water[i] > prevWall) {
+                result -= (height[i] + water[i]) - prevWall;
+            }
+        }
+        return result;
+    }
+}
