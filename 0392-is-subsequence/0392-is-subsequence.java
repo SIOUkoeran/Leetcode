1d8079474da7ceff610486637ea3f2e8efcc9b74
김민수 <alstnalsgud@gmail.com>
2023-10-02 22:01:48 +0900

    Time: 30 ms (6.20%), Space: 41.6 MB (5.07%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -7,11 +7,16 @@ class Solution {
         if (t.length() == 0)
             return false;
         int frontIdx = 0;
-        for (int i = 0; i < t.length(); i++) {
-            if (frontIdx == s.length())
+        int backIdx = s.length() - 1;
+        for (int i = 0; i < t.length() / 2 + 1; i++) {
+            System.out.println("frontIdx " + frontIdx + " backIdx " + backIdx);
+            if (frontIdx > backIdx)
                 return true;
             if (s.charAt(frontIdx) == t.charAt(i)) {
                 ++frontIdx;
+            }
+            if (s.charAt(backIdx) == t.charAt(t.length() - 1 - i)) {
+                --backIdx;
             }
         }
         return frontIdx == s.length() ? true : false;
