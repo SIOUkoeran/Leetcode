4b11b9d954efdedcac67e6bb55b0b61f50bd9c62
김민수 <alstnalsgud@gmail.com>
2023-10-02 15:49:51 +0900

    Time: 2 ms (99.77%), Space: 43.3 MB (98.64%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,32 +1,18 @@
 class Solution {
-    //((numRows - 1) - i )* 2
     public String convert(String s, int numRows) {
         if (numRows == 1)
             return s;
         StringBuilder sb = new StringBuilder();
-        
         for (int i = 0; i < numRows; i++) {
-            int cnt = 0;
-            for (int idx = i, offset = 1; idx < s.length(); idx += offset) {
+            for (int idx = i, offset = 1, cnt = 0; idx < s.length(); idx += offset) {
                 offset = 2 * (numRows - 1);
-                // first
-                if (i == 0) {
+                if (i == 0 || i == numRows - 1) {
                     sb.append(s.charAt(idx));
-                }
-                // last
-                else if (i == numRows - 1) {
+                } else {
+                    offset = cnt % 2 != 0
+                        ? 2 * i
+                        : offset - (2 * i);
                     sb.append(s.charAt(idx));
-                }
-                // general
-                else {
-                    if (cnt % 2 != 0) {
-                        offset = (2 * i);
-                        sb.append(s.charAt(idx));
-                    }
-                    else {
-                        offset -= (2 * i); 
-                        sb.append(s.charAt(idx));
-                    }
                 }
                 ++cnt;
             }
