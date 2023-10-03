fc8eb9a85366ebd68626b8da79536b8cf7336f5f
김민수 <alstnalsgud@gmail.com>
2023-10-03 23:12:21 +0900

    Time: 1700 ms (42.45%), Space: 44.7 MB (66.29%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        Map<String, Integer> dict = initDict(words);
+        int wordLength = words[0].length();
+        int totalLength = wordLength * words.length;
+        for (int i = 0; i <= s.length() - totalLength; i++) {
+            String possibleString = s.substring(i, totalLength + i);
+            int startIdx = 0;
+            int wordsIdx = 0;
+            Map<String, Integer> map = new HashMap<String, Integer>();
+            while (startIdx < totalLength) {
+                String w = possibleString.substring(startIdx, startIdx + wordLength);
+                if (map.containsKey(w))
+                    map.put(w, map.get(w) + 1);
+                else
+                    map.put(w, 1);
+                startIdx += wordLength;
+            }
+            if (map.equals(dict)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+    
+    private Map<String, Integer> initDict(String[] words) {
+        Map<String, Integer> dict = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            if (!dict.containsKey(words[i])) {
+                dict.put(words[i], 1);
+            }
+            else{
+                Integer cnt = dict.get(words[i]);
+                dict.put(words[i], cnt + 1);
+            }
+        }
+        return dict;
+    }
+}
